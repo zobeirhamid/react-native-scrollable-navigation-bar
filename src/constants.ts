dda38473f2721e68ef8fbf0066f4d3bb397fe595
Kevin <kevin@Mac-mini-de-Kevin.local>
2023-10-04 17:12:08 +0200

    support iPhone 15 Pro, Pro Max, Plus

--- src/constants.ts
+++ src/constants.ts
@@ -20,7 +20,9 @@ function isIphoneX() {
       dimen.height === 896 ||
       dimen.width === 896 ||
       dimen.height === 926 ||
-      dimen.width === 926)
+      dimen.width === 926 ||
+      dimen.height === 932 ||
+      dimen.width === 932)
   );
 }
 
